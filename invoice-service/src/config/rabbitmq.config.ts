2f03af32fab153d91580f3f5007c96265fdaf354
wikiccu <privatemx.2010@gmail.com>
2024-12-24 10:49:10 +0330

    taha+

--- invoice-service/src/config/rabbitmq.config.ts
+++
@@ -1,4 +0,0 @@
-export const RABBITMQ_CONFIG = {
-  uri: 'amqp://user:password@localhost:5672', // Replace with your RabbitMQ URI
-  queue: 'daily_sales_report',
-};
--- invoice-service/src/reports/reports.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ReportsService } from './reports.service';
-
-@Module({
-  providers: [ReportsService]
-})
-export class ReportsModule {}
